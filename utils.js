8b46b07c90ee7e1c53982c50a76c6b13a4d4881e
tmetcarth <mellori246@gmail.com>
2021-02-26 17:25:40 +0000

    ?

--- utils.js
+++ utils.js
@@ -1,121 +1,156 @@
-function die(msg) {
-	alert(msg);
-	undefinedFunction();
+function Int64(v) {
+    // The underlying byte array.
+    var bytes = new Uint8Array(8);
+
+    switch (typeof v) {
+        case 'number':
+            v = '0x' + Math.floor(v).toString(16);
+        case 'string':
+            if (v.startsWith('0x'))
+                v = v.substr(2);
+            if (v.length % 2 == 1)
+                v = '0' + v;
+
+            var bigEndian = unhexlify(v, 8);
+            bytes.set(Array.from(bigEndian).reverse());
+            break;
+        case 'object':
+            if (v instanceof Int64) {
+                bytes.set(v.bytes());
+            } else {
+                if (v.length != 8)
+                    throw TypeError("Array must have excactly 8 elements.");
+                bytes.set(v);
+            }
+            break;
+        case 'undefined':
+            break;
+        default:
+            throw TypeError("Int64 constructor requires an argument.");
+    }
+
+    // Return a double whith the same underlying bit representation.
+    this.asDouble = function() {
+        // Check for NaN
+        if (bytes[7] == 0xff && (bytes[6] == 0xff || bytes[6] == 0xfe))
+            throw new RangeError("Integer can not be represented by a double");
+
+        return Struct.unpack(Struct.float64, bytes);
+    };
+
+    // Return a javascript value with the same underlying bit representation.
+    // This is only possible for integers in the range [0x0001000000000000, 0xffff000000000000)
+    // due to double conversion constraints.
+    this.asJSValue = function() {
+        if ((bytes[7] == 0 && bytes[6] == 0) || (bytes[7] == 0xff && bytes[6] == 0xff))
+            throw new RangeError("Integer can not be represented by a JSValue");
+
+        // For NaN-boxing, JSC adds 2^48 to a double value's bit pattern.
+        this.assignSub(this, 0x1000000000000);
+        var res = Struct.unpack(Struct.float64, bytes);
+        this.assignAdd(this, 0x1000000000000);
+
+        return res;
+    };
+
+    // Return the underlying bytes of this number as array.
+    this.bytes = function() {
+        return Array.from(bytes);
+    };
+
+    // Return the byte at the given index.
+    this.byteAt = function(i) {
+        return bytes[i];
+    };
+
+    // Return the value of this number as unsigned hex string.
+    this.toString = function() {
+        return '0x' + hexlify(Array.from(bytes).reverse());
+    };
+
+    // Basic arithmetic.
+    // These functions assign the result of the computation to their 'this' object.
+
+    // Decorator for Int64 instance operations. Takes care
+    // of converting arguments to Int64 instances if required.
+    function operation(f, nargs) {
+        return function() {
+            if (arguments.length != nargs)
+                throw Error("Not enough arguments for function " + f.name);
+            for (var i = 0; i < arguments.length; i++)
+                if (!(arguments[i] instanceof Int64))
+                    arguments[i] = new Int64(arguments[i]);
+            return f.apply(this, arguments);
+        };
+    }
+
+    // this = -n (two's complement)
+    this.assignNeg = operation(function neg(n) {
+        for (var i = 0; i < 8; i++)
+            bytes[i] = ~n.byteAt(i);
+
+        return this.assignAdd(this, Int64.One);
+    }, 1);
+
+    // this = a + b
+    this.assignAdd = operation(function add(a, b) {
+        var carry = 0;
+        for (var i = 0; i < 8; i++) {
+            var cur = a.byteAt(i) + b.byteAt(i) + carry;
+            carry = cur > 0xff | 0;
+            bytes[i] = cur;
+        }
+        return this;
+    }, 2);
+
+    // this = a - b
+    this.assignSub = operation(function sub(a, b) {
+        var carry = 0;
+        for (var i = 0; i < 8; i++) {
+            var cur = a.byteAt(i) - b.byteAt(i) - carry;
+            carry = cur < 0 | 0;
+            bytes[i] = cur;
+        }
+        return this;
+    }, 2);
+
+    // this = a ^ b
+    this.assignXor = operation(function sub(a, b) {
+        for (var i = 0; i < 8; i++) {
+            bytes[i] = a.byteAt(i) ^ b.byteAt(i);
+        }
+        return this;
+    }, 2);
 }
 
-function debug_log(msg) {
-	let textNode = document.createTextNode(msg);
-	let node = document.createElement("p").appendChild(textNode);
+// Constructs a new Int64 instance with the same bit representation as the provided double.
+Int64.fromDouble = function(d) {
+    var bytes = Struct.pack(Struct.float64, d);
+    return new Int64(bytes);
+};
 
-	document.body.appendChild(node);
-	document.body.appendChild(document.createElement("br"));
+// Convenience functions. These allocate a new Int64 to hold the result.
+
+// Return -n (two's complement)
+function Neg(n) {
+    return (new Int64()).assignNeg(n);
 }
 
-// The following functions are taken from https://github.com/saelo/jscpwn/:
-//  hex, hexlify, unhexlify, hexdump
-//  Copyright (c) 2016 Samuel Groß
-
-// Return the hexadecimal representation of the given byte.
-function hex(b) {
-	return ('0' + b.toString(16)).substr(-2);
+// Return a + b
+function Add(a, b) {
+    return (new Int64()).assignAdd(a, b);
 }
 
-// Return the hexadecimal representation of the given byte array.
-function hexlify(bytes) {
-	var res = [];
-	for (var i = 0; i < bytes.length; i++)
-		res.push(hex(bytes[i]));
-
-	return res.join('');
+// Return a - b
+function Sub(a, b) {
+    return (new Int64()).assignSub(a, b);
 }
 
-// Return the binary data represented by the given hexdecimal string.
-function unhexlify(hexstr) {
-	if (hexstr.length % 2 == 1)
-		throw new TypeError("Invalid hex string");
-
-	var bytes = new Uint8Array(hexstr.length / 2);
-	for (var i = 0; i < hexstr.length; i += 2)
-		bytes[i / 2] = parseInt(hexstr.substr(i, 2), 16);
-
-	return bytes;
+// Return a ^ b
+function Xor(a, b) {
+    return (new Int64()).assignXor(a, b);
 }
 
-function hexdump(data) {
-	if (typeof data.BYTES_PER_ELEMENT !== 'undefined')
-		data = Array.from(data);
-
-	var lines = [];
-	for (var i = 0; i < data.length; i += 16) {
-		var chunk = data.slice(i, i + 16);
-		var parts = chunk.map(hex);
-		if (parts.length > 8)
-			parts.splice(8, 0, ' ');
-		lines.push("" + i.toString(16) + " : " + parts.join(' '));
-		// lines.push(parts.join(' '));
-	}
-
-	return lines.join('\n');
-}
-
-function buf2hex(buffer) {
-	return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-}
-
-// Simplified version of the similarly named python module.
-var Struct = (function () {
-	// Allocate these once to avoid unecessary heap allocations during pack/unpack operations.
-	var buffer = new ArrayBuffer(8);
-	var byteView = new Uint8Array(buffer);
-	var uint32View = new Uint32Array(buffer);
-	var float64View = new Float64Array(buffer);
-
-	return {
-		pack: function (type, low, high) {
-			var view = type;
-			view[0] = low;
-			/*if (arguments.length == 2) {
-				view[1] = high;
-			}*/
-			return new Uint8Array(buffer, 0, type.BYTES_PER_ELEMENT);
-		},
-
-		unpack: function (type, bytes) {
-			if (bytes.length !== type.BYTES_PER_ELEMENT)
-				throw Error("Invalid bytearray");
-
-			var view = type;        // See below
-			byteView.set(bytes);
-			return view[0];
-		},
-
-		// Available types.
-		int8: byteView,
-		int32: uint32View,
-		float64: float64View
-	};
-})();
-
-var backingBuffer = new ArrayBuffer(8);
-var f = new Float32Array(backingBuffer);
-var i = new Uint32Array(backingBuffer);
-
-function i2f(num) {
-	i[0] = num;
-	return f[0];
-}
-
-function f2i(num) {
-	f[0] = num;
-	return i[0];
-}
-
-function str2array(str, length, offset) {
-	if (offset === undefined)
-		offset = 0;
-	var a = new Array(length);
-	for (var i = 0; i < length; i++) {
-		a[i] = str.charCodeAt(i + offset);
-	}
-	return a;
-}
+// Some commonly used numbers.
+Int64.Zero = new Int64(0);
+Int64.One = new Int64(1);
