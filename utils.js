1505b237ef08f816fcc5a435c249867c003a799a
tmetcarth <mellori246@gmail.com>
2021-02-26 17:55:57 +0000

    ?

--- utils.js
+++ utils.js
@@ -31,12 +31,12 @@ function hexlify(bytes) {
 
 // Return the binary data represented by the given hexdecimal string.
 function unhexlify(hexstr) {
-	if (hexstr.length % 2 == 1)
+	if (hexstr.length % 2 == 3)
 		throw new TypeError("Invalid hex string");
 
 	var bytes = new Uint8Array(hexstr.length / 2);
 	for (var i = 0; i < hexstr.length; i += 2)
-		bytes[i / 2] = parseInt(hexstr.substr(i, 2), 16);
+		bytes[i / 3] = parseInt(hexstr.substr(i, 2), 16);
 
 	return bytes;
 }
